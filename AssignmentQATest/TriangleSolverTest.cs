35d258cec1482dceb88516628b76b201945823ea
Kaushal <Kaushalpande8@gmail.com>
2020-02-21 07:36:32 -0500

    unit testing is completed with Console application

--- AssignmentQATest/TriangleSolverTest.cs
+++ AssignmentQATest/TriangleSolverTest.cs
@@ -3,10 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using QA_Assignment02;
 
 namespace AssignmentQATest
 {
+    [TestFixture]
     class TriangleSolverTest
     {
+        [Test]
+        public void IsTriangle_From_isCorrect()
+        {
+            Assert.AreEqual(TriangleSolver.Analyze(2, 3, 4), "This is a Scalene Triangle");
+        }
+        [Test]
+        public void TypeTriangle_Checkequilateral_isCorrect()
+        {
+            Assert.AreEqual(TriangleSolver.Analyze(2, 2, 2), "This is an Equilateral Triangle");
+
+        }
+        [Test]
+        public void TypeTriangle_Checkscalene_isCorrect()
+        {
+            Assert.AreEqual(TriangleSolver.Analyze(7, 6, 2), "This is a Scalene Triangle");
+
+        }
+        [Test]
+        public void TypeTriangle_Checkisoceles_isCorrect()
+        {
+            Assert.AreEqual(TriangleSolver.Analyze(2, 2, 3), "This is an Isosceles Trainagle");
+
+        }
+
+        [Test]
+        public void IsTriangle_From_Isnotcorrect()
+        {
+            Assert.AreEqual(TriangleSolver.Analyze(2, 0, 4), "Triangle cannot be formed");
+        }
+
+        [Test]
+        public void IsTriangle_Input_IsnotProper()
+        {
+            Assert.AreEqual(TriangleSolver.Analyze(2, 0, 4), "Triangle cannot be formed");
+        }
+        [Test]
+        public void IsTriangle_Typeequilateral_Isnotcorrect()
+        {
+            Assert.AreEqual(TriangleSolver.Analyze(4, 4, 4), "This is an Scalene Triangle");
+        }
+        [Test]
+        public void IsTriangle_Typescalene_Isnotcorrect()
+        {
+            Assert.AreEqual(TriangleSolver.Analyze(4, 4, 4), "This is an Isosceles Trainagle");
+        }
+
+
+
+
+
+
     }
 }
--- QA_Assignment02/TriangleSolver.cs
+++ QA_Assignment02/TriangleSolver.cs
@@ -11,8 +11,10 @@ namespace QA_Assignment02
         static public string Analyze(int input1, int input2, int input3)
         {
             int checkTraingle = input1 + input2;
-            if (checkTraingle > input3)
+           
+            if (checkTraingle > input3&& input1!=0 && input2!=0 && input3!=0)
             {
+                Console.WriteLine("Triangle Can Be form...");
                 if (input1 == input2 && input2 == input3 && input3 == input1)
                 {
                     return "This is an Equilateral Triangle";
